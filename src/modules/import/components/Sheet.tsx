98ef21fb44c0c46f8842cb69afb86eceffb6737f
♦·Ariam·♦ <ariamfr0189@gmail.com>
2022-04-06 08:14:30 -0400

    feat(styles): update cells and header text color

--- src/modules/import/components/Sheet.tsx
+++ src/modules/import/components/Sheet.tsx
@@ -2,13 +2,30 @@ import '@ag-grid-community/all-modules/dist/styles/ag-grid.css';
 import '@ag-grid-community/all-modules/dist/styles/ag-theme-alpine-dark.css';
 import { AgGridReact } from '@ag-grid-community/react';
 import { AllModules, ColDef } from '@ag-grid-enterprise/all-modules';
-import { Box } from '@mui/material';
+import { Box, SxProps } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 import React, { useContext, useMemo } from 'react';
 import { ContextImportStore } from '../contexts/ContextImportStore';
 import cellEditor from './SelectEditor';
 
 // const ColumnsDefs: ColDef[] = [{ field: 'head' }, { field: 'map', editable: true, cellEditor }];
+
+const sx: SxProps = {
+  width: '100%',
+  height: '100%',
+  '& .ag-header-cell': {
+    '& .ag-header-cell-text': { color: '#b1b1b1' },
+    borderRight: `1px solid var(--ag-border-color, #68686e)`
+  },
+  '&.ag-theme-alpine-dark': {
+    '& .ag-cell': {
+      borderRightColor: `var(--ag-border-color, #68686e)`
+    },
+    '& .ag-row': {
+      color: `#b1b1b1`
+    }
+  }
+};
 
 const Sheet = () => {
   const store = useContext(ContextImportStore);
@@ -30,18 +47,7 @@ const Sheet = () => {
   }, [store.getOptions]);
 
   return (
-    <Box
-      className="ag-theme-alpine-dark"
-      sx={{
-        width: '100%',
-        height: '100%',
-        '& .ag-header-cell': {
-          borderRight: `1px solid var(--ag-border-color, #68686e)`
-        },
-        '&.ag-theme-alpine-dark .ag-cell': {
-          borderRightColor: `var(--ag-border-color, #68686e)`
-        }
-      }}>
+    <Box className="ag-theme-alpine-dark" sx={sx}>
       <AgGridReact
         defaultColDef={{
           sortable: true,
