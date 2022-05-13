536775ae5d4181088c9e25356fef0e23b14ca1b6
♦·Ariam·♦ <ariamfr0189@gmail.com>
2022-05-13 08:22:16 -0400

    feat(parser): update text normalization actions

--- src/modules/import/stores/utils/normalizeText.ts
+++ src/modules/import/stores/utils/normalizeText.ts
@@ -1,6 +1,16 @@
+/* eslint-disable no-control-regex */
 export function normalizeText(text: string): string {
   return text
+    .replace(/\x19 */gi, "'")
+    .replace(/：/gi, ': ')
+    .replace(/，/gi, ', ')
+    .replace(/\x14/gi, ',')
+    .replace(/–/gi, '-')
+    .replace(/\x1d ?/gi, '”')
+    .replace(/\x13 */gi, '-')
+    .replace(/\xa0 */gi, ' ')
+    .replace(/\x1c */gi, '“')
     .replace(/[·]/gi, '•')
-    .replace(/[^\w$*_+~.,()'"”!\-:@/•\n\t ]+/gi, '')
+    .replace(/[^A-Za-zÀ-ÿ\w$*_+~.,()'"”!\-:@/•\n\t ;‘’=]+/gi, '')
     .replace(/ +/gi, ' ');
 }
